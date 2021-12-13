25b78bd9248a19e147846ce075b1a234c8d7e129
dexter <>
2021-12-13 15:34:03 -0500

    Fix for log4j RMI exploit: Upgraded to log4j 2.15.0 with config changes

--- Java/neustar-coding/src/main/java/com/neustar/app/NeustarCategoryApplication.java
+++ Java/neustar-coding/src/main/java/com/neustar/app/NeustarCategoryApplication.java
@@ -6,8 +6,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.neustar.app.services.CategoryCounter;
 
